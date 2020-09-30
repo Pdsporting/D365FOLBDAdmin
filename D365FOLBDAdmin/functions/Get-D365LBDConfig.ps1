7a344ab73f5f8f60cbfb436a6a99cba43bd55212
unknown <38410725+stefanland@users.noreply.github.com>
2020-09-29 21:46:54 -0700

    trying to add servicefabric library import

--- D365FOLBDAdmin/functions/Get-D365LBDConfig.ps1
+++ D365FOLBDAdmin/functions/Get-D365LBDConfig.ps1
@@ -235,7 +235,9 @@
             try {
                 ##todo
                 Write-PSFMessage -Message "Trying to connect to $ConnectionEndpoint using $ServerCertificate" -Level Verbose
-                ##  $connection = Connect-ServiceFabricAutomatic -SFServerCertificate $ServerCertificate -SFConnectionEndpoint $ConnectionEndpoint 
+                ##  $connection = Connect-ServiceFabricAutomatic -SFServerCertificate $ServerCertificate -SFConnectionEndpoint $ConnectionEndpoint
+                $SFModuleSession = New-PSSession -ComputerName $OrchestratorServerName
+                Import-Module -Name ServiceFabric -PSSession $SFModuleSession 
                 $connection = Connect-ServiceFabricCluster -ConnectionEndpoint $ConnectionEndpoint -X509Credential -FindType FindByThumbprint -FindValue $ServerCertificate -ServerCertThumbprint $ServerCertificate -StoreLocation LocalMachine -StoreName My
                 $nodes = get-servicefabricnode | Where-Object { ($_.NodeType -eq "AOSNodeType") -or ($_.NodeType -eq "PrimaryNodeType") } #| Select-Object NodeName
                 Write-PSFMessage -message "Service Fabric Connected Nodes found $nodes" -Level Verbose
