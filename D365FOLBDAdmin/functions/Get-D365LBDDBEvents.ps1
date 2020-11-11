0db79ec40da1e428eb2f835fc7d5aa496c2552d3
unknown <38410725+stefanland@users.noreply.github.com>
2020-11-11 15:58:33 -0800

    fixed brackets

--- D365FOLBDAdmin/functions/Get-D365LBDDBEvents.ps1
+++ D365FOLBDAdmin/functions/Get-D365LBDDBEvents.ps1
@@ -14,20 +14,18 @@ function Get-D365LBDDBEvents {
             Write-PSFMessage -Level VeryVerbose -Message "$AXSFServerName $_"
             if ($_.Exception.Message -eq "No events were found that match the specified selection criteria") {
                 $LatestEventinLog = $null
-
             }
             if ($_.Exception.Message -eq "The RPC Server is unavailable") {
                 {           
                     Write-PSFMessage -Level Verbose -Message "The RPC Server is Unavailable trying WinRM"       
                     $LatestEventinLog = Invoke-Command -ComputerName $AXSFServerName -ScriptBlock { $(Get-EventLog -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents 1 -computername $AXSFServerName).TimeCreated }
                 }
-
             }
-            if (($LatestEventinLog -gt $LatestEventinAllLogs) -or (!$LatestEventinAllLogs)) {
-                $LatestEventinAllLogs = $LatestEventinLog
-                $ServerWithLatestLog = $AXSFServerName 
-                Write-PSFMessage -Level Verbose -Message "Server with latest log updated to $ServerWithLatestLog"
-            }
+        }
+        if (($LatestEventinLog -gt $LatestEventinAllLogs) -or (!$LatestEventinAllLogs)) {
+            $LatestEventinAllLogs = $LatestEventinLog
+            $ServerWithLatestLog = $AXSFServerName 
+            Write-PSFMessage -Level Verbose -Message "Server with latest log updated to $ServerWithLatestLog with a date time of $LatestEventinLog"
         }
         Write-PSFMessage -Level VeryVerbose -Message "Gathering from $ServerWithLatestLog"
         Get-WinEvent -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents $NumberofEvents -computername $ServerWithLatestLog
