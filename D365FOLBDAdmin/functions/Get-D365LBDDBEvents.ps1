61f131a35dc85bd20fe9c85bdf9c46362326734a
unknown <38410725+stefanland@users.noreply.github.com>
2020-11-11 15:00:17 -0800

    added exceptions for winevent

--- D365FOLBDAdmin/functions/Get-D365LBDDBEvents.ps1
+++ D365FOLBDAdmin/functions/Get-D365LBDDBEvents.ps1
@@ -8,27 +8,28 @@ function Get-D365LBDDBEvents {
     
     Foreach ($AXSFServerName in $config.AXSFServerNames) {
         try {
-            $latestEventinlog = $(Get-WinEvent -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents 1 -computername $AXSFServerName -ErrorAction Stop).TimeCreated
+            $LatestEventinLog = $(Get-WinEvent -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents 1 -computername $AXSFServerName -ErrorAction Stop).TimeCreated
         }
         catch {
             Write-PSFMessage -Level VeryVerbose -Message "$AXSFServerName $_"
-            if ($_.Exception.Message -eq "No events were found that match the specified  selection criteria") {
-                $latestEventinlog = $null
+            if ($_.Exception.Message -eq "No events were found that match the specified selection criteria") {
+                $LatestEventinLog = $null
 
             }
-            if ($_.Exception.Message -eq "The RPC Server is Unavailable trying WinRM") {
-                {                  
-                    $latestEventinlog = Invoke-Command -ComputerName $AXSFServerName -ScriptBlock { $(Get-EventLog -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents 1 -computername $AXSFServerName).TimeCreated }
+            if ($_.Exception.Message -eq "The RPC Server is unavailable") {
+                {           
+                    Write-PSFMessage -Level Verbose -Message "The RPC Server is Unavailable trying WinRM"       
+                    $LatestEventinLog = Invoke-Command -ComputerName $AXSFServerName -ScriptBlock { $(Get-EventLog -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents 1 -computername $AXSFServerName).TimeCreated }
                 }
 
             }
-            if (($latestEventinlog -gt $latesteventinalllogs) -or (!$latesteventinalllogs)) {
-                $latesteventinalllogs = $latestEventinlog
-                $serverwithlatestlog = $AXSFServerName 
-                Write-PSFMessage -Level Verbose -Message "Server with latest log updated to $Serverwithlatestlog"
+            if (($LatestEventinLog -gt $LatestEventinAllLogs) -or (!$LatestEventinAllLogs)) {
+                $LatestEventinAllLogs = $LatestEventinLog
+                $ServerWithLatestLog = $AXSFServerName 
+                Write-PSFMessage -Level Verbose -Message "Server with latest log updated to $ServerWithLatestLog"
             }
         }
-        Write-PSFMessage -Level Verbose -Message "Gathering from $serverwithlatestlog"
-        Get-WinEvent -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents $NumberofEvents -computername $serverwithlatestlog
+        Write-PSFMessage -Level VeryVerbose -Message "Gathering from $ServerWithLatestLog"
+        Get-WinEvent -LogName Microsoft-Dynamics-AX-DatabaseSynchronize/Operational -maxevents $NumberofEvents -computername $ServerWithLatestLog
     }
 }
