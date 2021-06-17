9c3d1d66acbc6b933a4357fd5d90d3c2fc0d37d4
unknown <38410725+stefanland@users.noreply.github.com>
2021-06-17 14:22:01 -0700

    fixed typo in remove assets

--- D365FOLBDAdmin/functions/Remove-D365LBDSFOldAssets.ps1
+++ D365FOLBDAdmin/functions/Remove-D365LBDSFOldAssets.ps1
@@ -25,11 +25,32 @@ function Remove-D365LBDSFOldAssets {
             Stop-PSFFunction -Message "Error: Number of Assets to keep must be 2 or larger as you should always keep the main and backup" -EnableException $True  
         }
         $AssetsFolderinAgentShareLocation = join-path -Path $Config.AgentShareLocation -ChildPath "\assets"
-        $Onedayold = (get-date).AddDays(-1)
+        $Onedayold = $(get-date).AddDays(-1)
         $AlreadyDeployedAssetIDInWPFolder = $Config.DeploymentAssetIDinWPFolder
         $StartTime = Get-Date
+        Write-PSFMessage -Level Verbose -Message "Checking for invalid assets in $AssetsFolderinAgentShareLocation"
+        $AssetFolders = Get-ChildItem $AssetsFolderinAgentShareLocation | Where-Object { $_.Name -ne "chk" -and $_.Name -ne "topology.xml" -and $_.Name -ne "$AlreadyDeployedAssetIDInWPFolder" -and $_.CreateDate -lt $Onedayold -and $_.Name -ne "ControlFile.txt" } 
+        foreach ($AssetFolder  in $AssetFolders ) {
+            $StandaloneSetupZip = $null
+            $StandaloneSetupZip = Get-ChildItem "$($AssetFolder.Fullname)\*\*\Packages\*\StandaloneSetup.zip"
+            if (!$StandaloneSetupZip) {
+                Write-PSFMessage -Message "$AssetFolder is invalid no StandaloneSetup found - deleting" -Level VeryVerbose
+                Get-ChildItem $AssetFolder.Fullname | Remove-Item -Recurse -Force
+                Get-ChildItem $AssetsFolderinAgentShareLocation | Where-object { $_.Name -eq $AssetFolder } | Remove-Item -Recurse -Force
+            }
+            else {
+                if ($StandaloneSetupZip.Length -eq 0) {
+                    Write-PSFMessage -Message "Standalone zip in $AssetFolder is invalid - deleting" -Level VeryVerbose
+                    Get-ChildItem $AssetFolder.Fullname | Remove-Item -Recurse -Force
+                    Get-ChildItem $AssetsFolderinAgentShareLocation | Where-object { $_.Name -eq $AssetFolder } | Remove-Item -Recurse -Force
+                }
+                else {
+                    Write-PSFMessage -Message "Standalone zip in $AssetFolder is VALID" -Level Verbose
+                }
+            }
+        }
         Write-PSFMessage -Level Verbose -Message "Starting Clean on $AssetsFolderinAgentShareLocation"
-        $FilesThatAreBeingDeleted = Get-ChildItem $AssetsFolderinAgentShareLocation | Where-Object { $_.Name -ne "chk" -and $_.Name -ne "toplogy.xml" -and $_.Name -ne "$AlreadyDeployedAssetIDInWPFolder" -and $_.LastWriteTime -lt $Onedayold -and $_.Name -ne "ControlFile.txt" } | Sort-Object LastWriteTime | Select-Object -SkipLast $NumberofAssetsToKeep
+        $FilesThatAreBeingDeleted = Get-ChildItem $AssetsFolderinAgentShareLocation | Where-Object { $_.Name -ne "chk" -and $_.Name -ne "topology.xml" -and $_.Name -ne "$AlreadyDeployedAssetIDInWPFolder" -and $_.CreateDate -lt $Onedayold -and $_.Name -ne "ControlFile.txt" } | Sort-Object LastWriteTime | Select-Object -SkipLast $NumberofAssetsToKeep
         $FileCount = $FilesThatAreBeingDeleted.Count
         if ($FileCount -or $FileCount -ne 0) {
             $FilesThatAreBeingDeleted.FullName | Remove-Item -Force -Recurse
