82ba8e0e1d65319b8c385baaeaea760f669f44aa
unknown <38410725+stefanland@users.noreply.github.com>
2021-07-06 13:57:03 -0700

    Fixed bug with no config for remove apps

--- D365FOLBDAdmin/functions/Remove-D365LBDStuckApps.ps1
+++ D365FOLBDAdmin/functions/Remove-D365LBDStuckApps.ps1
@@ -6,7 +6,10 @@ function Remove-D365LBDStuckApps {
         [string]$SFServerCertificate,
         [string]$SFConnectionEndpoint,
         [string]$AgentShareLocation,
-        [PSFComputer]$ComputerName = "$env:COMPUTERNAME"
+        [PSFComputer]$ComputerName = "$env:COMPUTERNAME",
+        [Parameter(ParameterSetName = 'Config',
+            ValueFromPipeline = $True)]
+        [psobject]$Config
     )
     BEGIN {
     }
@@ -19,21 +22,35 @@ function Remove-D365LBDStuckApps {
             $cachedconfigfile = Join-path $($config.AgentShareLocation) -ChildPath "scripts\config.xml" 
             $config = Get-D365LBDConfig -ConfigImportFromFile $cachedconfigfile
         }
+        if ($SFServerCertificate)
+        {
+            Connect-ServiceFabricCluster -ConnectionEndpoint $SFConnectionEndpoint -X509Credential -FindType FindByThumbprint -FindValue $SFServerCertificate -ServerCertThumbprint $SFServerCertificate
+        }
+        else{
+            Connect-ServiceFabricCluster -ConnectionEndpoint $config.SFConnectionEndpoint -X509Credential -FindType FindByThumbprint -FindValue $config.SFServerCertificate -ServerCertThumbprint $config.SFServerCertificate
+        }
 
-        Connect-ServiceFabricCluster -ConnectionEndpoint $config.SFConnectionEndpoint -X509Credential -FindType FindByThumbprint -FindValue $config.SFServerCertificate -ServerCertThumbprint $config.SFServerCertificate
-
-        $environmentwp = get-childitem $(Join-path $config.AgentShareLocation -ChildPath "\wp")
-        $archivefolder = $(Join-path $config.AgentShareLocation -ChildPath "\archive")
+        if ($AgentShareLocation) {
+            $environmentwp = get-childitem $(Join-path $AgentShareLocation -ChildPath "\wp")
+            $archivefolder = $(Join-path $AgentShareLocation -ChildPath "\archive")
+        }
+        else {
+            $environmentwp = get-childitem $(Join-path $config.AgentShareLocation -ChildPath "\wp")
+            $archivefolder = $(Join-path $config.AgentShareLocation -ChildPath "\archive")
+        }
+        
         if ((Test-Path $archivefolder) -eq $false) {
             Write-PSFMessage -Message "Creating archive folder" -Level Verbose
             mkdir $archivefolder
         }
         else {
             Write-PSFMessage -Message "Archive folder already exists" -Level Verbose
-            Get-ChildItem $environmentwp.FullName -Recurse | Remove-Item -Force
+            if (!$environmentwp) {
+                Write-PSFMessage -Level VeryVerbose -Message "WP Folder already cleaned up"
+            }
         }
         
-        Write-PSFMessage -Message "Deleting applications" -Level Verbose
+        Write-PSFMessage -Message "Deleting applications inside of Service Fabric" -Level Verbose
 
         $applicationNamesToIgnore = @('fabric:/LocalAgent', 'fabric:/Agent-Monitoring', 'fabric:/Agent-LBDTelemetry')
         $applicationTypeNamesToIgnore = @('MonitoringAgentAppType-Agent', 'LocalAgentType', 'LBDTelemetryType-Agent')
@@ -46,8 +63,12 @@ function Remove-D365LBDStuckApps {
             Where-Object { $_.ApplicationTypeName -notin $applicationTypeNamesToIgnore } | `
             Unregister-ServiceFabricApplicationType -Force
 
+        if (!$environmentwp) {
+        }
+        else {
             Write-PSFMessage "Moving $($environmentwp.FullName) to $archivefolder " -Level VeryVerbose
-        Move-Item -Path $environmentwp.FullName -Destination $archivefolder -Force -Verbose
+            Move-Item -Path $environmentwp.FullName -Destination $archivefolder -Force -Verbose
+        }
     
         Write-PSFMessage -Level Verbose -Message "Trigger deployment/retry in LCS"
     }
