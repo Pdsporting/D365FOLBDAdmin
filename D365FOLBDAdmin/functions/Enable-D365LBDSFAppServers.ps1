5073c465f9de0581eac217f94c8991facbec06bf
unknown <38410725+stefanland@users.noreply.github.com>
2021-09-07 12:11:47 -0700

    added logic to check if config object is invalid p2

--- D365FOLBDAdmin/functions/Enable-D365LBDSFAppServers.ps1
+++ D365FOLBDAdmin/functions/Enable-D365LBDSFAppServers.ps1
@@ -42,6 +42,7 @@ function Enable-D365LBDSFAppServers {
     } 
     PROCESS {
         if (!$Config -or $Config.OrchestratorServerNames.Count -eq 0) {
+            Write-PSFMessage -Level VeryVerbose -Message "Config not defined or Config is invalid. Trying to Get new config using $ComputerName"
             $Config = Get-D365LBDConfig -ComputerName $ComputerName
         }
         [int]$count = 0
